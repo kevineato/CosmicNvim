13a2912daf6f730d000976c6615fa799eb81a07d
kevineato <kevineato@users.noreply.github.com>
2022-02-07 15:16:29 -0600

    Remap all Floaterm commands.

--- lua/cosmic/plugins/terminal/mappings.lua
+++ lua/cosmic/plugins/terminal/mappings.lua
@@ -1,8 +1,8 @@
 local map = require('cosmic.utils').map
 
-map('n', '<C-l>', ':FloatermToggle<CR>')
-map('t', '<C-l>', [[<C-\><C-n>]])
-map('t', '<C-w>l', [[<C-\><C-n>:FloatermNext<CR>]])
-map('t', '<C-w>h', [[<C-\><C-n>:FloatermPrev<CR>]])
-map('t', '<C-w>n', [[<C-\><C-n>:FloatermNew<CR>]])
-map('t', '<C-w>c', [[<C-\><C-n>:FloatermKill<CR>]])
+map('n', '<C-t>', '<Cmd>FloatermToggle<CR>')
+map('t', '<C-t>', [[<C-\><C-n>]])
+map('t', [[<C-\>l]], [[<C-\><C-n><Cmd>FloatermNext<CR>]])
+map('t', [[<C-\>h]], [[<C-\><C-n><Cmd>FloatermPrev<CR>]])
+map('t', [[<C-\>n]], [[<C-\><C-n><Cmd>FloatermNew<CR>]])
+map('t', [[<C-\>c]], [[<C-\><C-n><Cmd>FloatermKill<CR>]])
