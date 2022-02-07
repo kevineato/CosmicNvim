d11513c0c8c84f1f51dd8582f7822c484107e916
kevineato <kevineato@users.noreply.github.com>
2022-02-07 15:15:51 -0600

    Remap lsp_signature in normal mode.

--- lua/cosmic/lsp/mappings.lua
+++ lua/cosmic/lsp/mappings.lua
@@ -30,7 +30,7 @@ function M.init(client, bufnr)
   buf_map(bufnr, 'v', '<leader>gf', '<cmd>lua vim.lsp.buf.range_formatting()<cr>')
 
   -- signature help
-  buf_map(bufnr, 'n', '<C-K>', '<cmd>lua require("lsp_signature").signature()<cr>')
+  buf_map(bufnr, 'n', '<leader>K', '<cmd>lua require("lsp_signature").toggle_float_win()<cr>')
 
   -- lsp workspace
   buf_map(bufnr, 'n', '<leader>wd', '<cmd>Telescope diagnostics<cr>')
