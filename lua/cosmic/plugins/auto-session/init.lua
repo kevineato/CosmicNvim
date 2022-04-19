bcda11cb889625d8bd9f17fe74be5f433371ad45
kevineato <kevineato@users.noreply.github.com>
2022-04-19 17:47:14 -0500

    Fix aut-session pre-save cmds.

--- lua/cosmic/plugins/auto-session/init.lua
+++ lua/cosmic/plugins/auto-session/init.lua
@@ -3,8 +3,6 @@ local u = require("cosmic.utils")
 
 local defaults = {
     pre_save_cmds = {
-        "Neotree close",
-        "NvimTreeClose",
         "cclose",
         "lua vim.notify.dismiss()",
     },
@@ -12,6 +10,14 @@ local defaults = {
     auto_save_enabled = true,
     auto_restore_enabled = false,
 }
+
+if vim.fn.exists(":Neotree") > 0 then
+    table.insert(defaults.pre_save_cmds, "Neotree close")
+end
+
+if vim.fn.exists(":NvimTreeClose") > 0 then
+    table.insert(defaults.pre_save_cmds, "NvimTreeClose")
+end
 
 require("auto-session").setup(u.merge(defaults, config.auto_session or {}))
 require("cosmic.plugins.auto-session.mappings")
