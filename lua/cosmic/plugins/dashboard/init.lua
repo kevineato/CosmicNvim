00590d739d42f291dd6e6e7139967590781165ad
kevineato <kevineato@users.noreply.github.com>
2022-08-02 12:00:50 -0500

    fix(dashboard): fix highlight colors

--- lua/cosmic/plugins/dashboard/init.lua
+++ lua/cosmic/plugins/dashboard/init.lua
@@ -1,5 +1,10 @@
 local icons = require("cosmic.theme.icons")
 local db = require("dashboard")
+local colors = require("cosmic.theme.colors")
+
+vim.api.nvim_set_hl(0, "DashboardHeader", { fg = colors.normal })
+vim.api.nvim_set_hl(0, "DashboardCenter", { fg = colors.insert })
+vim.api.nvim_set_hl(0, "DashboardShortCut", { fg = colors.visual })
 
 db.custom_header = {
     "",
@@ -24,54 +29,54 @@ db.session_directory = vim.fn.stdpath("data") .. "/sessions"
 db.custom_center = {
     {
         icon = icons.file1 .. " ",
+        icon_hl = { fg = colors.hint },
         desc = "Find File                      ",
-        shortcut = "<Leader>ff",
+        shortcut = "LDR f f",
         action = "lua require('telescope.builtin').find_files()",
     },
     {
         icon = icons.file1 .. " ",
+        icon_hl = { fg = colors.hint },
         desc = "Find MRU                       ",
-        shortcut = "<Leader>fo",
+        shortcut = "LDR f o",
         action = "lua require('telescope.builtin').oldfiles()",
     },
     {
         icon = icons.file1 .. " ",
+        icon_hl = { fg = colors.hint },
         desc = "Find MRU in CWD                ",
-        shortcut = "<Leader>FO",
+        shortcut = "LDR F O",
         action = "lua require('telescope.builtin').oldfiles({ only_cwd = true })",
     },
     {
         icon = icons.file2 .. " ",
+        icon_hl = { fg = colors.hint },
         desc = "File Manager                   ",
-        shortcut = "<C-n>     ",
+        shortcut = "CTRL n ",
         action = "Neotree left toggle reveal",
     },
     {
         icon = icons.word .. " ",
+        icon_hl = { fg = colors.hint },
         desc = "Live Grep                      ",
-        shortcut = "<Leader>fs",
+        shortcut = "LDR f s",
         action = "lua require('telescope.builtin').live_grep()",
     },
     {
         icon = icons.clock .. " ",
+        icon_hl = { fg = colors.hint },
         desc = "Load Session                   ",
-        shortcut = "<Leader>sl",
+        shortcut = "LDR s l",
         action = "lua vim.cmd(':silent RestoreSession')",
     },
     {
         icon = icons.error .. " ",
+        icon_hl = { fg = colors.hint },
         desc = "Quit                           ",
-        shortcut = "q         ",
+        shortcut = "q      ",
         action = "quit",
     },
 }
-
-local colors = require("cosmic.theme.colors")
-
-vim.cmd("hi default DashboardHeader guifg=" .. colors.normal)
-vim.cmd("hi default DashboardCenter guifg=" .. colors.insert)
-vim.cmd("hi default DashboardCenterIcon guifg=" .. colors.hint)
-vim.cmd("hi default DashboardShortCut guifg=" .. colors.visual)
 
 vim.cmd([[
     augroup dashboard_keymap
