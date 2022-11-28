274abaab1ebd9449a5e7236ea09dcba155c28a9a
kevineato <kevineato@users.noreply.github.com>
2022-11-28 10:13:20 -0600

    fix: update comment type names

--- lua/cosmic/plugins/comments/init.lua
+++ lua/cosmic/plugins/comments/init.lua
@@ -5,7 +5,7 @@ require("Comment").setup(u.merge({
     pre_hook = function(ctx)
         local U = require("Comment.utils")
         local location = nil
-        if ctx.ctype == U.ctype.block then
+        if ctx.ctype == U.ctype.blockwise then
             location =
                 require("ts_context_commentstring.utils").get_cursor_location()
         elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
@@ -14,7 +14,8 @@ require("Comment").setup(u.merge({
         end
 
         return require("ts_context_commentstring.internal").calculate_commentstring({
-            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+            key = ctx.ctype == U.ctype.linewise and "__default"
+                or "__multiline",
             location = location,
         })
     end,
