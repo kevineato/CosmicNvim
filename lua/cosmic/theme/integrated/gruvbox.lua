18a6e81989bcb83a863645fb89283efc52625b02
kevineato <kevineato@users.noreply.github.com>
2023-09-25 10:47:14 -0500

    fix(theme): update gruvbox colors config

--- lua/cosmic/theme/integrated/gruvbox.lua
+++ lua/cosmic/theme/integrated/gruvbox.lua
@@ -1,23 +1,26 @@
-local theme = require("gruvbox.palette").colors
+local palette = require("gruvbox.palette")
+local light = palette.get_base_colors({}, "light", "hard")
+local dark = palette.get_base_colors({}, "dark", "")
+dark.neutral_orange = "#d65d0e"
 
 local colors = {
-    white = theme.light0_hard,
-    bg = theme.dark0,
-    bg_highlight = theme.dark1,
-    normal = theme.neutral_yellow,
-    insert = theme.neutral_green,
-    command = theme.neutral_orange,
-    visual = theme.neutral_purple,
-    replace = theme.neutral_red,
-    diffAdd = theme.neutral_green,
-    diffModified = theme.neutral_orange,
-    diffDeleted = theme.neutral_red,
-    trace = theme.neutral_orange,
-    hint = theme.neutral_blue,
-    info = theme.neutral_green,
-    error = theme.neutral_red,
-    warn = theme.neutral_orange,
-    floatBorder = theme.dark3,
-    selection_caret = theme.neutral_blue,
+    white = light.bg0,
+    bg = dark.bg0,
+    bg_highlight = dark.bg1,
+    normal = dark.neutral_yellow,
+    insert = dark.neutral_green,
+    command = dark.neutral_orange,
+    visual = dark.neutral_purple,
+    replace = dark.neutral_red,
+    diffAdd = dark.neutral_green,
+    diffModified = dark.neutral_orange,
+    diffDeleted = dark.neutral_red,
+    trace = dark.neutral_orange,
+    hint = dark.neutral_blue,
+    info = dark.neutral_green,
+    error = dark.neutral_red,
+    warn = dark.neutral_orange,
+    floatBorder = dark.bg3,
+    selection_caret = dark.neutral_blue,
 }
 return colors
